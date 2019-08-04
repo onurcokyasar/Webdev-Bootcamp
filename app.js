5ffb490bf4aef666d58b6baa80c36aadc29aa7a2
Onur Cokyasar <onurcokyasar@outlook.com>
2019-08-04 12:50:13 +0000

    Add mongoDB Atlas

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes 	  = require("./routes/comments"),
 	indexRoutes 	  = require("./routes/index");
 
 // seedDB(); // Seed the database
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://onurcokyasar:123@cluster0-3oiad.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+// abc12343oban21
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
